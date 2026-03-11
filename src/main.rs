e57749514ba8c0525f6e1ae0f927a43476f5d271
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-03-11 22:31:39 +1100

    better temp parser: 20.9

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::{
-    os::fd::AsRawFd, ptr::slice_from_raw_parts_mut, str::from_utf8_unchecked,
-};
+use std::{os::fd::AsRawFd, ptr::slice_from_raw_parts_mut, str::from_utf8_unchecked};
 
 use mmap::{MapOption, MemoryMap};
 use rapidhash::{HashMapExt, RapidHashMap};
@@ -14,13 +12,15 @@ fn main() {
     )
     .unwrap();
     let slice = slice_from_raw_parts_mut(mmap.data(), file_len);
-    let contents = unsafe { from_utf8_unchecked(slice.as_ref().unwrap()) }.trim();
+    let bytes = unsafe { slice.as_ref().unwrap() };
+    let contents = unsafe { from_utf8_unchecked(bytes) };
 
     // (min, max, len, total)
     let mut map: RapidHashMap<&str, (f64, f64, usize, f64)> = RapidHashMap::with_capacity(1000);
+    // TODO: raw byte reading
     for line in contents.lines() {
         let (station, temperature) = split_stat(line);
-        let temperature = parse_temperature(temperature);
+        let temperature = parse_temperature(temperature.as_bytes());
 
         let entry = map
             .entry(station)
@@ -51,30 +51,60 @@ fn main() {
 
 #[inline(always)]
 fn split_stat(stat: &str) -> (&str, &str) {
-    let index = stat.chars().rev().position(|c| c == ';').unwrap();
-    stat.split_at(stat.len() - index)
+    let index = stat.bytes().rposition(|c| c == b';').unwrap();
+    let (left, right) = stat.split_at(index);
+    (left, &right[1..])
 }
 
 #[inline(always)]
-fn parse_temperature(temperature: &str) -> f64 {
-    let mut chars = temperature.chars().rev().peekable();
-    let decimal = chars.next().unwrap() as i32 - '0' as i32;
-    let negative = unsafe { *temperature.as_ptr() == b'-' };
+fn parse_temperature(temperature: &[u8]) -> f64 {
+    let len = temperature.len();
+    let negative = unsafe { *temperature.get_unchecked(0) == b'-' };
+    let decimal = unsafe { *temperature.get_unchecked(len - 1) } - b'0';
+    let ndigits = len - 2 - negative as usize;
+    let start_index = negative as usize;
 
-    // skip over the '.' character
-    let _ = chars.next();
-    let mut whole = 0;
-    while let Some(c) = chars.next() {
-        if chars.peek().is_none() && negative {
-            break;
-        }
-
-        whole += c as i32 - '0' as i32;
-    }
-
-    if negative {
-        -((whole as f64) + decimal as f64 / 10.)
+    // either 1 or 2
+    let whole = if ndigits == 1 {
+        (unsafe { *temperature.get_unchecked(start_index) } - b'0')
     } else {
-        (whole as f64) + decimal as f64 / 10.
+        (unsafe { *temperature.get_unchecked(start_index) } - b'0') * 10
+            + (unsafe { *temperature.get_unchecked(start_index + 1) } - b'0')
+    };
+
+    ((whole as f64) + decimal as f64 / 10.) * (1 - 2 * negative as i32) as f64
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_parse_temperature() {
+        // negative two-digit whole
+        assert_eq!(parse_temperature(b"-99.9"), -99.9);
+        assert_eq!(parse_temperature(b"-99.0"), -99.0);
+        assert_eq!(parse_temperature(b"-50.5"), -50.5);
+        assert_eq!(parse_temperature(b"-10.1"), -10.1);
+        assert_eq!(parse_temperature(b"-12.3"), -12.3);
+        // negative one-digit whole
+        assert_eq!(parse_temperature(b"-9.9"), -9.9);
+        assert_eq!(parse_temperature(b"-9.0"), -9.0);
+        assert_eq!(parse_temperature(b"-1.5"), -1.5);
+        assert_eq!(parse_temperature(b"-0.1"), -0.1);
+        // zero
+        assert_eq!(parse_temperature(b"0.0"), 0.0);
+        // positive one-digit whole
+        assert_eq!(parse_temperature(b"0.1"), 0.1);
+        assert_eq!(parse_temperature(b"1.5"), 1.5);
+        assert_eq!(parse_temperature(b"9.0"), 9.0);
+        assert_eq!(parse_temperature(b"9.9"), 9.9);
+        // positive two-digit whole
+        assert_eq!(parse_temperature(b"10.0"), 10.0);
+        assert_eq!(parse_temperature(b"12.3"), 12.3);
+        assert_eq!(parse_temperature(b"50.5"), 50.5);
+        assert_eq!(parse_temperature(b"99.0"), 99.0);
+        assert_eq!(parse_temperature(b"99.9"), 99.9);
     }
 }
